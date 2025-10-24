5780830086f0e318baf8f54920995ac27613a750
Baurzhan Abenov <baurzhan.aben@gmail.com>
2025-10-24 11:22:39 -0700

    Initial commit

---
+++ PlaygroundApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlaygroundApp
+//
+//  Created by Baurzhan on 10/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlaygroundApp/PlaygroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundAppApp.swift
+//  PlaygroundApp
+//
+//  Created by Baurzhan on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
